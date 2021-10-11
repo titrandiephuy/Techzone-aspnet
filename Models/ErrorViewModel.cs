f2a63b09afe7c8259fc6ae2fb35daca2e3fc4ae1
Diep Huy <titrandiephuy@gmail.com>
2021-10-11 22:02:03 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TechZone.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
